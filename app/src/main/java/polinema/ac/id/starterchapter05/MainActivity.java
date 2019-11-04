2ceecf85fc7237a9f57549959caed0c869f72e21
alifalamiin <yoursweetestn@gmail.com>
2019-11-04 14:38:02 +0700

    commited

--- app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
+++ app/src/main/java/polinema/ac/id/starterchapter05/MainActivity.java
@@ -2,7 +2,12 @@ package polinema.ac.id.starterchapter05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import polinema.ac.id.starterchapter05.activities.DynamicActivity;
+import polinema.ac.id.starterchapter05.activities.StaticActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void handlerClickStaticFragment(View view) {
+        Intent intent = new Intent(this, StaticActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerClickDynamicFragment(View view) {
+        Intent intent = new Intent(this, DynamicActivity.class);
+        startActivity(intent);
+    }
+
+    public void handlerClicPraktikumFragment(View view) {
+    }
+
+
 }
